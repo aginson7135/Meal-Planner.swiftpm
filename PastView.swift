be3ac961314b7a16be7df5108d13dff9b9ee1b24
Haasini Police <hpolice5296@stu.d214.org>
2024-04-19 14:30:20 -0500

    I added a new view called PastView.

---
+++ PastView.swift
@@ -0,0 +1,19 @@
+//
+//  PastView.swift
+//  Meal Planner
+//
+//  Created by Haasini Police on 4/19/24.
+//
+
+import Foundation
+import SwiftUI
+struct PastView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
