e0692ad6b6c1c7de63e5b45a526fbc7227cff298
Aneena Ginson <aginson7135@stu.d214.org>
2024-04-19 14:28:29 -0500

    added some code in dietary info view

--- DietaryInformationView.swift
+++ DietaryInformationView.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+import SwiftUI
+struct DietaryInformationView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
