5cba4df7671db987f2d016b3524ff51ccb7a1d43
Haasini Police <hpolice5296@stu.d214.org>
2024-04-19 14:44:53 -0500

    I made a new view called MealView.

---
+++ MealView.swift
@@ -0,0 +1,19 @@
+//
+//  MealView.swift
+//  Meal Planner
+//
+//  Created by Haasini Police on 4/19/24.
+//
+
+import Foundation
+import SwiftUI
+struct MealView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
