ed77466da635c145f165b56f37a01f7dcc8ddf96
Aneena Ginson <aginson7135@stu.d214.org>
2024-04-19 14:19:56 -0500

    added view input meals

---
+++ InputMeals.swift
@@ -0,0 +1,19 @@
+//
+//  InputMeals.swift
+//  Meal Planner
+//
+//  Created by Aneena Ginson on 4/19/24.
+//
+
+import Foundation
+import SwiftUI
+struct InputMeals: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
