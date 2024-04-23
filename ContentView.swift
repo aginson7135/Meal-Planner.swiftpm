493f1ce6cae5b6c9f2663f5c35854ec5ee095332
abbytharakan <atharakan5365@stu.d214.org>
2024-04-23 14:16:57 -0500

    link to mealview

--- ContentView.swift
+++ ContentView.swift
@@ -4,6 +4,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Meal Planner")
+            NavigationView {
+                NavigationLink{
+                    MealView()
+                }label: {
+                    Text("Link to MealView")
+                }
+            }
         }
     }
 }
