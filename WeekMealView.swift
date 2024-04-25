3bae04904697b98ccf5acbb3bd767a896180964e
Haasini Police <hpolice5296@stu.d214.org>
2024-04-25 14:03:49 -0500

    I made another subview for mealview.

---
+++ WeekMealView.swift
@@ -0,0 +1,19 @@
+//
+//  WeekMealView.swift
+//  Meal Planner
+//
+//  Created by Haasini Police on 4/25/24.
+//
+
+import Foundation
+import SwiftUI
+struct WeekMealView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
