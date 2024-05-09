40003666abfa1d296e4599f52b452b2b075c948a
Haasini Police <hpolice5296@stu.d214.org>
2024-05-09 14:09:50 -0500

    i made a functioning button

--- MealView.swift
+++ MealView.swift
@@ -28,9 +28,9 @@ struct MealView: View {
                 let newInputMeals = InputMeals(time: mealTime, meal: mealMeal, cals: mealCal)
                 meals.append(newInputMeals)
                 
-                mealTime = ""
-                mealCal = ""
-                mealMeal = ""
+              //  mealTime = ""
+               // mealCal = ""
+               // mealMeal = ""
             }
            
                 
