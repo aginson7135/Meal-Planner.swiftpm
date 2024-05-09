6f9977e13ebfd07c3d84e3bed44654ac929b6d53
Aneena Ginson <aginson7135@stu.d214.org>
2024-05-09 14:06:39 -0500

    modifiers

--- DietaryInformationView.swift
+++ DietaryInformationView.swift
@@ -42,38 +42,73 @@ struct DietaryInformationView: View {
                 .bold()
                
             Text("TIPS FOR EATING HEALTHIER")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Base your meals on higher fibre starchy carbohydrates")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Eat lots of fruit and vegetables")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Eat more fish, including a portion of oily fish")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Cut down on saturated fat and sugar")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Eat less salt: no more than 6g a day for adults")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Get active and be a healthy weight")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Drink enough water so that you are not dehydrated")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Make sure to eat breakfast")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Try not to eat too much sugar")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Omega-3 Fats Are Crucial and Most People Don’t Get Enough")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("There Is No Perfect Diet for Everyone")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Artificial Trans Fats Are Very Unhealthy")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("It Is Critical to Avoid a Vitamin D Deficiency")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Refined Carbohydrates Are Bad for You")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Supplements Can Never Fully Replace Real Foods")
-            
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
             Text("Unprocessed Food Is Healthiest")
+                .foregroundColor(.purple)
+                .font(.custom("Didot", size: 30))
+                .bold()
         }
     }
 }
