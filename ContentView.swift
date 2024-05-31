34d39eb8c0b92ad0a3b36f223b82efe1e0c4b5fd
abbytharakan <atharakan5365@stu.d214.org>
2024-05-31 11:13:39 -0500

    it didn't work

--- ContentView.swift
+++ ContentView.swift
@@ -34,101 +34,95 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
             
             Button {
-                       spin.toggle()
-                   }label: {
-                       Text("Spin Image")
-                           .foregroundColor(.green)
-                           .bold()
-                           .font(.system(size: 30))
-                   }
+                spin.toggle()
+            }label: {
+                Text("Spin Image")
+                    .foregroundColor(.green)
+                    .bold()
+                    .font(.system(size: 30))
+            }
             Image("mealpic")
                 .resizable()
                 .frame(width: 300, height: 250)
                 .rotationEffect(.degrees(spin ? 360 : 0))
-                            .animation(.easeInOut(duration: 1), value: spin)
-               
-                    }
-                    
-                    NavigationStack{
-                        
-                        NavigationLink ("Meal Plan"){
-                            MealFinalView()
-                                .foregroundColor(.purple)
-                                .bold()
-                            
-                            // mealMeal: mealMeal,mealCal: mealCal, mealTime: mealTime,  meals: $meals
-                            
-                            
-                        }
-                        Spacer().frame(height: 30)
-                        
-                        NavigationLink ("Motives")
-                        {
-                            MotivesFinalView(currentMotives: $currentMotives)
-                                .foregroundColor(.purple)
-                                .bold()
-                            
-                            
-                        }
-                        Spacer().frame(height: 30)
-                        
-                        
-                        NavigationLink ("More Information") {
-                            DietaryInformationView()
-                                .foregroundColor(.purple)
-                                .bold()
-                        }
-                        Spacer().frame(height: 30)
-                        
-                        
-                        NavigationLink("Recipe Links") {
-                            RecipieLinks()
-                                .foregroundColor(.purple)
-                                .bold()
-                        }
-                        Spacer().frame(height: 30)
-                        
-                        //   .alert("GO EAT", isPresented: $ShowAlert) {
-                        //     Button("Okay") {
-                        //       motive = ""
-                        // }
-                        //   }
-                        
-                        Button(action: {
-                            self.ShowAlert = true
-                        }, label: {
-                            Text("Motivate Me!")
-                            
-                        })
-                        .alert("\(currentMotives.motive)", isPresented: $ShowAlert) { //print($currentMotives.motive)
-                            Text("Hello")
-                            Button("Yay!") {
-                                // motive = ""
-                            }
-                            Spacer().frame(height: 30)
-                            
-                        }
-                        Image(systemName: "fanblades")
-                                 .font(.system(size: 80))
-                                 .foregroundColor(.green)
-                                 .rotationEffect(.degrees(rotate))
-                               
-                                 .onAppear {
-                                     withAnimation(.linear(duration: 1)
-                                         .speed(0.1).repeatForever(autoreverses: false)) {
-                                             rotate = 360.0
-                                         }
-                                    
-                      
-                    }
-       
-                    
-                  
-                }
+                .animation(.easeInOut(duration: 1), value: spin)
             
         }
         
+        NavigationStack{
+            
+            NavigationLink ("Meal Plan"){
+                MealFinalView()
+                    .foregroundColor(.purple)
+                    .bold()
+                
+                // mealMeal: mealMeal,mealCal: mealCal, mealTime: mealTime,  meals: $meals
+                
+                
+            }
+            Spacer().frame(height: 30)
+            
+            NavigationLink ("Motives")
+            {
+                MotivesFinalView(currentMotives: $currentMotives)
+                    .foregroundColor(.purple)
+                    .bold()
+                
+                
+            }
+            Spacer().frame(height: 30)
+            
+            
+            NavigationLink ("More Information") {
+                DietaryInformationView()
+                    .foregroundColor(.purple)
+                    .bold()
+            }
+            Spacer().frame(height: 30)
+            
+            
+            NavigationLink("Recipe Links") {
+                RecipieLinks()
+                    .foregroundColor(.purple)
+                    .bold()
+            }
+            Spacer().frame(height: 30)
+            
+            //   .alert("GO EAT", isPresented: $ShowAlert) {
+            //     Button("Okay") {
+            //       motive = ""
+            // }
+            //   }
+            
+            Button(action: {
+                self.ShowAlert = true
+            }, label: {
+                Text("Motivate Me!")
+                
+            })
+            .alert("\(currentMotives.motive)", isPresented: $ShowAlert) { //print($currentMotives.motive)
+                Text("Hello")
+                Button("Yay!") {
+                    // motive = ""
+                }
+                Spacer().frame(height: 30)
+                
+            }
+            Image(systemName: "fanblades")
+                .font(.system(size: 80))
+                .foregroundColor(.green)
+                .rotationEffect(.degrees(rotate))
+            
+                .onAppear {
+                    withAnimation(.linear(duration: 1)
+                        .speed(0.1).repeatForever(autoreverses: false)) {
+                            rotate = 360.0
+                        }
+                    
+                }
+        }
     }
+}
     
     
 // }
